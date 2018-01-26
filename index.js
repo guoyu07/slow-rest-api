e64d0dba3a8141f36dea057fa72bf1c48a2245bb
swami701 <swami701search@gmail.com>
2018-01-26 12:26:14 +0800

    4th soln node-core not restify

--- index.js
+++ index.js
@@ -8,41 +8,41 @@ global.logger = bunyan.createLogger({
 var etag = require('./etag')
 var pkg = JSON.stringify(require('./package.json'))
 
-var restify = require('restify')
-var server = restify.createServer()
+var http = require('http')
 var count = 1
+var server = http.createServer((req, res) => {
+  switch (req.url) {
+    case '/a':
+      var tag = etag(pkg + ++count)
 
-server.get('/a', function (req, res, next) {
-  var tag = etag(pkg + ++count)
+      if (!(tag instanceof Error)) {
+        res.setHeader('ETag', tag)
+      }
 
-  if (!(tag instanceof Error)) {
-    res.setHeader('ETag', tag)
+      res.end(pkg)
+      break;
+    case '/b':
+      var tag = etag({ entity: pkg + ++count, algorithm: 'sha256' })
+
+      if (!(tag instanceof Error)) {
+        res.setHeader('ETag', tag)
+      }
+
+      res.end(pkg)
+      break
+    case '/c':
+      var tag = etag(pkg + ++count, { algorithm: 'sha512WithRsaEncryption' })
+
+      if (!(tag instanceof Error)) {
+        res.setHeader('ETag', tag)
+      }
+
+      res.end(pkg)
+      break
+    default:
+      res.end('Not a valid route')
+      break;
   }
-
-  res.send(pkg)
-  return next()
-})
-
-server.get('/b', function (req, res, next) {
-  var tag = etag({entity: pkg + ++count, algorithm: 'sha256'})
-
-  if (!(tag instanceof Error)) {
-    res.setHeader('ETag', tag)
-  }
-
-  res.send(pkg)
-  return next()
-})
-
-server.get('/c', function (req, res, next) {
-  var tag = etag(pkg + ++count, {algorithm: 'sha512WithRsaEncryption'})
-
-  if (!(tag instanceof Error)) {
-    res.setHeader('ETag', tag)
-  }
-
-  res.send(pkg)
-  return next()
 })
 
 server.listen(3000)
@@ -52,7 +52,7 @@ var signal = 'SIGINT'
 // Cleanly shut down process on SIGINT to ensure that perf-<pid>.map gets flushed
 process.on(signal, onSignal)
 
-function onSignal () {
+function onSignal() {
   console.error('count', count)
   // IMPORTANT to log on stderr, to not clutter stdout which is purely for data, i.e. dtrace stacks
   console.error('Caught', signal, ', shutting down.')
